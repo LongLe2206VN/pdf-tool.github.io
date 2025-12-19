7b22a3d0ddb33cff729d44c8b50654e4c1bcc93e
home <long.le@gs25.com.vn>
2025-12-19 17:35:40 +0700

    add docs

---
+++ docs/_next/static/q88703w_cHGTRaOZUDT65/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pdf-tool2//_next/:path+",
+        "destination": "/pdf-tool2/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
